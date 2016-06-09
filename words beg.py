5e3a7ab4b95f23bd17a4d3a6de60abb7ae57b3ce
Ofir Hershko <odh1234321@gmail.com>
2016-06-09 16:34:38 +0300

    Add files via upload

--- words beg.py
+++ words beg.py
@@ -12,21 +12,28 @@ unique_dict = {}
 word_bag = []
 word_countr = 0
 X, y = load_dataset('tweets.csv')
-for sen in X:
+
+for i in range(int(0.75 * len(X))):
+    sen = X[i]
     words_list = sen.split()
     for word in words_list:
         if word in unique_dict:
-            unique_dict[word] += 1
+            unique_dict[word][0] += 1
+            unique_dict[word][y[i] + 1] += 1
         else:
-            unique_dict[word] = 1
+            unique_dict[word] = [0]*11
+            unique_dict[word][0] += 1
         word_bag.append(word)
 print('unique length ' + str(len(unique_dict)))
 print('number of words ' + str(len(word_bag)))
-# print('words: ')
-# for key in unique_dict.keys():
-#     print(key + " --> " + str(unique_dict[key]))
 
-sorted_unique_dictionary = sorted(unique_dict, key = unique_dict.get)
+#prints all the words
 print('words: ')
-for i in range(len(sorted_unique_dictionary)):
-    print(sorted_unique_dictionary[-(i + 1)] + " --> " + str(unique_dict[sorted_unique_dictionary[-(i + 1)]]))
+for key in unique_dict.keys():
+    print(key + " --> " + str(unique_dict[key]))
+
+#trying to sort
+sorted_unique_dictionary = sorted(unique_dict, key = lambda item: item[1])
+# print('words: ')
+# for i in range(len(sorted_unique_dictionary)):
+#     print(sorted_unique_dictionary[-(i + 1)] + " --> " + str(unique_dict[sorted_unique_dictionary[-(i + 1)]]))
