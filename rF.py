4528bd8c68f6d7df26275b2748eeef026f6ddef4
halden718 <attiaa13@gmail.com>
2016-06-09 22:55:09 +0300

    Add files via upload

---
+++ rF.py
@@ -0,0 +1,63 @@
+# Standard scientific Python imports
+import matplotlib.pyplot as plt
+
+# Import datasets, classifiers and performance metrics
+from sklearn import datasets, svm, metrics
+from sklearn import ensemble
+from tools import *
+
+def setLabels(y,i):
+      return [1 if j == i else 0 for j in y]
+
+
+def setG(y, i):
+    return [1 if j == i else 0 for j in y]
+
+# special_words = get_special_words(1000)
+# X,y = load_dataset()
+# trainX,trainY=subSet(X,y,12000)
+# gX = list(subX)
+# gY = [yToGroup(t) for t in subY.tolist()]
+# dataX = [features_vec(t,special_words) for t in gX]
+# dataY = gY
+# dataX = [features_vec(t,special_words) for t in subX]
+# dataY = subY
+
+special_words = get_special_words(1000)
+X,y = load_dataset()
+trainX,trainY=subSet(X,y,15000)
+dataX = [features_vec(t,special_words) for t in trainX]
+dataY = trainY
+testX,testY=subSet(X,y,5000,15000)
+dataTestX = [features_vec(t,special_words) for t in testX]
+dataTestY = testY
+
+# Create a classifier
+classifier = ensemble.RandomForestClassifier(10)
+
+# We learn the digits on the first half of the digits
+classifier.fit(dataX, dataY)
+
+# Now predict the value of the digit on the second half:
+expected = dataTestY
+predicted = classifier.predict(dataTestX)
+
+print("Classification report for classifier %s:\n%s\n"
+      % (classifier, metrics.classification_report(expected, predicted)))
+
+for i in range(10):
+    iyTrain = setLabels(dataY,i)
+    iyTest = setLabels(dataTestY,i)
+
+    # Create a classifier
+    classifier = ensemble.RandomForestClassifier(10)
+
+    # We learn the digits on the first half of the digits
+    classifier.fit(dataX, iyTrain)
+
+    # Now predict the value of the digit on the second half:
+    expected = iyTest
+    predicted = classifier.predict(dataTestX)
+
+    print("Classification report for classifier %s:\n%s\n"
+          % (classifier, metrics.classification_report(expected, predicted)))
---
+++ tools.py
@@ -0,0 +1,142 @@
+import pandas
+import operator
+import re
+import numpy
+
+
+def yToGroup(y):
+    if y in [0, 1, 2, 3]:
+        return 0
+    if y in [4, 7, 9]:
+        return 1
+    if y in [6, 8]:
+        return 2
+    if y in [5]:
+        return 3
+
+
+def load_dataset(filename='tweets.csv'):
+    train = pandas.read_csv(filename, header=None)
+    X, y = train[1], train[0]
+    X = X.tolist()
+    y = y.tolist()
+    return X, y
+
+
+def subSet(X, y, s, sh = 0):
+    return X[sh:s+sh], y[sh:s+sh]
+
+
+def num_of_twit_tripplets(twit):
+    tripplets = 0
+    words_list = twit.split()
+    for word in words_list:
+        tripplets += num_of_word_tripplets(word)
+    return tripplets
+
+
+def num_of_word_tripplets(word):
+    return len(re.findall("([A-Za-z])\1\1+", word))
+
+
+def num_of_tripplets(twit):
+    return len(re.findall("([A-Za-z])\1\1+", word))
+
+
+def num_of_upper_letters(word):
+    upper_case_letter = 0
+    for letter in list(word):
+        upper_case_letter += letter == letter.upper()
+    return upper_case_letter
+
+
+def num_of_upper_word(twit):
+    upper_case_words = 0
+    words_list = twit.split()
+    for word in words_list:
+        upper_case_words += word == word.upper()
+    return upper_case_words
+
+
+def num_of_dots(twit):
+    return len(re.findall("..+", twit))
+
+
+def num_of_word(twit, word):
+    return twit.count(word)
+
+
+def twit_len(twit):
+    return len(twit.split(' '))
+
+
+def isIn(a, b):
+    if a in b:
+        return 1
+    return 0
+
+
+def features_vec(twit,num):
+    return [len(twit),num_of_word(twit, '#'), num_of_word(twit, '@'), num_of_word(twit, '!'), 'bama' in twit, 'nald' in twit,
+            'lary' in twit, num_of_dots(twit), twit_len(twit), 'resident' in twit, 'ardash' in twit, '&amp' in twit,
+            '1989' in twit, 'ball' in twit, 'Soul' in twit] + significant_words(twit,num)
+    return [num_of_word(twit, '#'), num_of_word(twit, '@'), num_of_word(twit, '!'), 'bama' in twit, 'nald' in twit,
+            'lary' in twit, num_of_dots(twit), twit_len(twit), 'resident' in twit, 'ardash' in twit, '&amp' in twit,
+            '1989' in twit,'ball' in twit, 'Soul' in twit]
+    return [num_of_word(twit, '#'), num_of_word(twit, '@'), num_of_word(twit, '!'), 'bama' in twit, 'nald' in twit,
+            'lary' in twit, num_of_dots(twit), twit_len(twit)]
+
+def my_key(entry):
+    return entry[1][11]
+
+
+def get_special_words(num_of_special_words=100):
+    unique_dict = {}
+    word_bag = []
+    word_countr = 0
+    X, y = load_dataset('tweets.csv')
+    num_posts_by_poster = [0] * 10
+
+    for i in range(int(0.5 * len(X))):
+        sen = X[i]
+        words_list = sen.split()
+        num_posts_by_poster[y[i]] += 1
+        for word in words_list:
+            word = word.lower()
+            if word in unique_dict:
+                unique_dict[word][0] += 1
+                unique_dict[word][y[i] + 1] += 1
+            else:
+                unique_dict[word] = [0] * 12
+                unique_dict[word][0] += 1
+            word_bag.append(word)
+
+    for entry in unique_dict:
+        values = unique_dict[entry][1:-1]
+        for i in range(len(values)):
+            values[i] /= num_posts_by_poster[i]
+            unique_dict[entry][i + 1] = values[i]
+        m = numpy.max(values)
+        mean = numpy.mean(values)
+        unique_dict[entry][11] = m - mean
+
+    sorted_unique_dictionary = sorted(unique_dict.items(), key = my_key, reverse = True)
+
+    special_word = [None] * num_of_special_words
+    for i in range(min(num_of_special_words, len(sorted_unique_dictionary))):
+        var = numpy.var(sorted_unique_dictionary[i][1][1:])
+        values = sorted_unique_dictionary[i][1][1:11]
+        # for j in range(len(values)):
+        # values[j] /= posts_num[j]
+        special_word[i] = sorted_unique_dictionary[i][0]
+    return special_word
+
+def significant_words(twit,special_words):
+	return [(special_words[i] in twit.lower()) for i in range(len(special_words))]
+
+# def get_batch(X,y,i):
+#     return tf.constant(X[i*BATCH_SIZE:(i+1)*BATCH_SIZE]),tf.constant(y[i*BATCH_SIZE:(i+1)*BATCH_SIZE])
+# INPUT_SIZE = 10
+# OUTPUT_SIZE = 10
+# BATCH_SIZE = 10
+# NUM_BATCHES = 10
